b0c1c5def53075b4dd0bb36b97e3516bde6a7b20
Teles <jose.maciel@appmax.com.br>
2025-07-24 07:16:10 -0300

    style: Comment out unused imports and HTTPS configuration in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,17 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import fs from "fs";
-import path from "path";
+// import fs from "fs";
+// import path from "path";
 
 export default defineConfig({
   plugins: [vue()],
   server: {
     host: "localhost",
     port: 5173,
-    https: {
-      key: fs.readFileSync(path.resolve(__dirname, "certs/localhost-key.pem")),
-      cert: fs.readFileSync(path.resolve(__dirname, "certs/localhost.pem")),
-    },
+    // https: {
+    //   key: fs.readFileSync(path.resolve(__dirname, "certs/localhost-key.pem")),
+    //   cert: fs.readFileSync(path.resolve(__dirname, "certs/localhost.pem")),
+    // },
     proxy: {
       "/api": "http://localhost:4000",
     },
